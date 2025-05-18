7c113ba06ab8d657b0e739b07a4a5a4c0fc13c27
AsuraMzn <ribubaucha@gmail.com>
2025-05-18 15:21:06 +0545

    add layout demo

---
+++ RelativeLayoutDemo/app/src/main/java/com/mobileprogramming/relativelayoutdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.mobileprogramming.relativelayoutdemo;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
